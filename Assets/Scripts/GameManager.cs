6db631a99b6afcf6cad0d2fe38812b9deadb37ff
MINTHONGVOLLEY <thongdnmhe176561@fpt.edu.vn>
2024-09-14 00:11:55 +0700

    Update Project

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,31 +1,55 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private int score;
+    private int yourScore;
 
-    public Player     player;
-    public Text       scoreText;
-    public GameObject playButton;
-    public GameObject gameOver;
-
+    public           Player           player;
+    public           Text             scoreText;
+    public           Text             yourScoreText;
+    public           GameObject       playButton;
+    public           GameObject       pannel;
+    public           TMP_Text         inputNameText;
+    public           GameObject       gameWaiting;
+    [SerializeField] HighscoreHandler highscoreHandler;
+    
+    // create a list to save a score list.
+    List<HighscoreElement> highscoreList = new List<HighscoreElement>();
+    
+    // create a file name.
+    [SerializeField] string filename;
+    
     private void Awake()
     {
         Application.targetFrameRate = 60;
         
         this.Pause();
     }
+    
+    private void Start()
+    {
+        this.highscoreList = FileHandler.ReadListFromJSON<HighscoreElement>(filename);
+    
+        if (this.highscoreList == null)
+        {
+            this.highscoreList = new List<HighscoreElement>();
+        }
+    }
+
 
     public void Play()
     {
         score = 0;
         scoreText.text = score.ToString();
         playButton.SetActive(false);
-        gameOver.SetActive(false);
 
         Time.timeScale = 1f;
         this.player.enabled = true;
@@ -45,14 +69,36 @@ public class GameManager : MonoBehaviour
     
     public void GameOver()
     {
-        this.gameOver.SetActive(true);
-        this.playButton.SetActive(true);
-        
+        this.playButton.SetActive(false);
+        this.yourScoreText.text = score.ToString();
         this.Pause();
+        this.pannel.SetActive(true);
     }
+    public void SaveScore()
+    {
+        string playerName = inputNameText.text.Trim();
+
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            this.highscoreList.Add(new HighscoreElement(this.score, playerName));
+            //this.highscoreHandler.AddHighscoreIfPossible(new HighscoreElement(this.score, playerName));
+            FileHandler.SaveToJSON<HighscoreElement>(this.highscoreList, filename);
+            this.pannel.SetActive(false);
+            this.gameWaiting.SetActive(true);
+        }
+    }
+    
+    
     public void IncreaseScore()
     {
         this.score++;
         this.scoreText.text = this.score.ToString();
+    }
+
+    public void ReplayGame()
+    {
+        Debug.Log("be clicked!");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        this.gameWaiting.SetActive(false);
     }
 }
